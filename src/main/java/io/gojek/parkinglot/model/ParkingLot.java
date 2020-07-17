fa641e9f833646122b06a6f1043083d39e2d184e
sudhanshu <sudhanshu97gupta@gmail.com>
2020-07-17 23:08:44 +0400

    fix the slot number as string

--- src/main/java/io/gojek/parkinglot/model/ParkingLot.java
+++ src/main/java/io/gojek/parkinglot/model/ParkingLot.java
@@ -8,11 +8,18 @@ import java.util.Map;
 public interface ParkingLot<T extends Vehicle> extends Serializable {
 
     String park(T vehicle);
+
     boolean leave(String slotId);
+
     Map<String, Vehicle> getAllOccupiedSlots();
+
     int getAvailableSlot();
+
     List<String> getSlotNumbersByVehicleColor(String color);
+
     List<String> getRegistrationNumbersByVehicleColor(String color);
+
     String getSlotNumberByRegistrationNumber(String registrationNumber);
+
     void clear();
 }
